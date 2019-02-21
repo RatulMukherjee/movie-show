ca9a179634e2bf774173c28030a647845d05169f
Ratul Mukherjee <ratul.lamborghini@gmail.com>
2019-02-21 17:56:29 +0530

    Added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
+const routes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'register' , component: RegisterComponent},
+  {path: 'login' , component: LoginComponent},
+];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes),
+   ],
+   exports: [RouterModule],
 })
 export class AppRoutingModule { }
+export const routingComponents = [ RegisterComponent , LoginComponent, HomeComponent ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './shared/menu/menu.component';
-import { LoginComponent } from './login/login.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
@@ -14,7 +13,8 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatButtonModule} from '@angular/material/button';
 import {AppRoutingModule} from './app-routing.module';
-import { RegisterComponent } from './register/register.component';
+import {routingComponents} from './app-routing.module';
+
 
 
 
@@ -22,8 +22,7 @@ import { RegisterComponent } from './register/register.component';
   declarations: [
     AppComponent,
     MenuComponent,
-    LoginComponent,
-    RegisterComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
